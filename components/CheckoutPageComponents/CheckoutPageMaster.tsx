7937200795c577f2a9f75603d9754926a1b48d97
karan <karan@8848digital.com>
2024-09-10 17:15:05 +0530

    feat: :sparkles: chckout web comps created

--- components/CheckoutPageComponents/CheckoutPageMaster.tsx
+++ components/CheckoutPageComponents/CheckoutPageMaster.tsx
@@ -1,7 +1,17 @@
+import { useSelector } from 'react-redux';
 import useCheckout from '../../hooks/CheckoutPageHook/useCheckout';
+import { SelectedFilterLangDataFromStore } from '../../store/slices/general_slices/selected-multilanguage-slice';
+import WebCheckout from './WebCheckout';
 
 export default function CheckoutPageMaster() {
-  //   const { addressLoading, addressError, shippingAddress } = useGetUserAddresses();
+  const { selectedLanguageData }: any = useSelector(SelectedFilterLangDataFromStore);
   useCheckout();
-  return <div></div>;
+  return (
+    <div className="container margin_from_nav">
+      <div className="page_heading ps-4 ms-1">
+        <h4 className="color-black mb-0">{selectedLanguageData?.checkout_page}</h4>
+        <WebCheckout selectedLanguageData={selectedLanguageData} />
+      </div>
+    </div>
+  );
 }
