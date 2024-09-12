0b5bacf593a7c533a4ed00c3b2184070277f437a
amir rai <amir@8848Digital.com>
2024-09-12 14:14:05 +0530

    feat: developemnt of my orders page

--- components/MyOrder/PlacedOrders.tsx
+++ components/MyOrder/PlacedOrders.tsx
@@ -1,41 +1,26 @@
 import MyOrderCard from '../../cards/MyOrderCard';
 import NoDataFound from '../NoRecordFound';
+import { Placeholder } from 'react-bootstrap';
 
 function PlacedOrders({ selectedMultiLangData, isLoading, orderListData }: any) {
   return (
     <>
       <div role="tabpanel" aria-hidden="false">
-        <div className="row mb-4 fs-14">
-          <div className="col-lg-12">
-            <div className="row">
-              <div className="col-lg-2 col-sm-4 col-6">
-                <select
-                  className="form-select w-auto "
-                  // onChange={handleHistoryDate}
-                  // value={history}
-                >
-                  <option value="this_month">{selectedMultiLangData?.this_month}</option>
-                  <option value="last_30_days">{selectedMultiLangData?.last_30_days}</option>
-                  <option value="past_3_months">{selectedMultiLangData?.past_3_months}</option>
-                  <option value="2022">2022</option>
-                  <option value="2021">2021</option>
-                  <option value="2020">2020</option>
-                </select>
-              </div>
-              <div className="col text-end me-2 order-myorder-margin">
-                <p className="mb-0 order-ptag">
-                  <span className="bold">{orderListData?.length}</span> {selectedMultiLangData?.orders}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
         {isLoading === true ? (
           <div className="row justify-content-center">
             {[...Array(10)].map(() => (
               <>
-                <div className="col-lg-12 mx-3">
-                  <div className="spinner-border" role="status" />
+                <div className="card">
+                  <Placeholder animation="glow">
+                    <div className="row">
+                      <div className="col-4">
+                        <Placeholder className="" style={{ width: '100%', height: 200 }} />
+                      </div>
+                      <div className="col-8">
+                        <Placeholder className="" style={{ width: '100%', height: 200 }} />
+                      </div>
+                    </div>
+                  </Placeholder>
                 </div>
               </>
             ))}
@@ -48,9 +33,9 @@ function PlacedOrders({ selectedMultiLangData, isLoading, orderListData }: any) 
                   orderListData
                     ?.filter((items: any) => items?.payment_status !== 'Cancelled')
                     ?.map((data: any, i: any) => (
-                      <div className="row color-black fs-14" key={i}>
+                      <div className="row color-black fs-14 fw-light" key={i}>
                         <div className="col-lg-12">
-                          <div className="order_card cart_table mb-2 card">
+                          <div className="order_card cart_table mb-2 card ">
                             <MyOrderCard data={data} selectedMultiLangData={selectedMultiLangData} />
                           </div>
                         </div>
