5a1e16bb6a9f312af9a0ba1c25a95ecdc18c9c64
amir rai <amir@8848Digital.com>
2024-09-14 17:22:20 +0530

    feat: development og my profile page

--- components/profile/ProfileMaster.tsx
+++ components/profile/ProfileMaster.tsx
@@ -1,289 +1,166 @@
 import React from 'react';
 import { Col, Nav, Row, Tab, Table } from 'react-bootstrap';
 import { FaUser, FaLocationDot, FaTags, FaCreditCard, FaCalendarDays } from 'react-icons/fa6';
+import useGetUserDetails from '../../hooks/UserProfileHook/useGetUserDetails';
+import { useSelector } from 'react-redux';
+import selectedMultilanguageSlice, {
+  SelectedFilterLangDataFromStore,
+} from '../../store/slices/general_slices/selected-multilanguage-slice';
+import EnquiryHistory from './EnquiryHistory';
 
 const ProfileMaster = () => {
-  return (
-    <div className="container">
-      <div className="row my-3">
-        <div className="col-12">
-          <h4 className="fs-3 fw-bold">My Account</h4>
-        </div>
-      </div>
-      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-        <Row className="my-2">
-          <Col sm={3}>
-            <Nav variant="" className="flex-column" style={{ position: 'sticky', top: '64px', bottom: '0' }}>
-              <Nav.Item className="border">
-                <Nav.Link eventKey="personalDetails" className="fs-3 d-flex justify-content-center flex-column align-items-center">
-                  <div>
-                    <FaUser />
-                  </div>
-                  <div>User Details</div>
-                </Nav.Link>
-              </Nav.Item>
-              {/* <Nav.Item className="border">
-                <Nav.Link eventKey="address" className="fs-3 d-flex justify-content-center flex-column align-items-center">
-                  <div>
-                    <FaLocationDot />
-                  </div>
-                  <div>Address</div>
-                </Nav.Link>
-              </Nav.Item> */}
-              {/* <Nav.Item className="border">
-                <Nav.Link eventKey="offers" className="fs-3 d-flex justify-content-center flex-column align-items-center">
-                  <div>
-                    <FaTags />
-                  </div>
-                  <div> Offers </div>
-                </Nav.Link>
-              </Nav.Item> */}
-              {/* <Nav.Item className="border">
-                <Nav.Link eventKey="payment" className="fs-3 d-flex justify-content-center flex-column align-items-center">
-                  <div>
-                    <FaCreditCard />
-                  </div>
-                  <div>Payment</div>
-                </Nav.Link>
-              </Nav.Item> */}
-              <Nav.Item className="border">
-                <Nav.Link eventKey="enquiryHistory" className="fs-3 d-flex justify-content-center flex-column align-items-center">
-                  <div>
-                    <FaCalendarDays />
-                  </div>
-                  <div>Enquiry History</div>
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-          <Col sm={9}>
-            <Tab.Content>
-              <Tab.Pane eventKey="personalDetails">
-                <div className="card">
-                  <div className="row mx-2">
-                    <div className="col-3 fw-bold fs-5">
-                      <p className="mt-1">Comapnay Name</p>
-                    </div>
-                    <div className="col-9">
-                      <p className="mt-2">848 Digital</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="card">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="row mx-1 border-end">
-                        <div className="col-12">
-                          <h4 className="fw-bold">Shipping Address</h4>
+  const { userData, isLoading, errorMessage } = useGetUserDetails();
+  const { selectedLanguageData }: any = useSelector(SelectedFilterLangDataFromStore);
+
+  const arrForPersonalDetails = [
+    { label: selectedLanguageData?.user_name || '', value: userData?.profile_details?.customer_name || '' },
+    { label: selectedLanguageData?.company_name || '', value: userData?.profile_details?.customer_name || '' },
+    { label: selectedLanguageData?.mobile_number || '', value: userData?.profile_details?.contact_no || '' },
+    { label: selectedLanguageData?.email || '', value: userData?.profile_details?.email || '' },
+  ];
+  const arrForAddressDetails = [
+    {
+      label: selectedLanguageData?.shipping_addresses || '',
+      value: [
+        { label: selectedLanguageData?.name || '', value: userData?.shipping_address?.name || '' },
+        { label: selectedLanguageData?.email || '', value: userData?.shipping_address?.user_id?.email || '' },
+        { label: selectedLanguageData?.mobile_number, value: userData?.shipping_address?.user_id?.mobile_no || '' },
+        { label: selectedLanguageData?.address, value: userData?.shipping_address?.full_address } || '',
+        { label: selectedLanguageData?.postal_code, value: userData?.shipping_address?.postal_code || '' },
+        { label: selectedLanguageData?.state, value: userData?.shipping_address?.state || '' },
+        { label: selectedLanguageData?.city, value: userData?.shipping_address?.city || '' },
+        { label: selectedLanguageData?.country, value: userData?.shipping_address?.country || '' },
+      ],
+    },
+    {
+      label: selectedLanguageData?.billing_addresses || '',
+      value: [
+        { label: selectedLanguageData?.name, value: userData?.billing_address?.name || '' },
+        { label: selectedLanguageData?.email, value: userData?.billing_address?.user_id?.email || '' },
+        { label: selectedLanguageData?.mobile_number, value: userData?.billing_address?.user_id?.mobile_no || '' },
+        { label: selectedLanguageData?.address, value: userData?.billing_address?.full_address || '' },
+        { label: selectedLanguageData?.postal_code, value: userData?.billing_address?.postal_code || '' },
+        { label: selectedLanguageData?.state, value: userData?.billing_address?.state || '' },
+        { label: selectedLanguageData?.city, value: userData?.billing_address?.city || '' },
+        { label: selectedLanguageData?.country, value: userData?.billing_address?.country || '' },
+      ],
+    },
+  ];
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (userData && Object?.keys(userData)?.length > 0) {
+    return (
+      <div className="container-fluid">
+        <div className="mx-md-5">
+          <div className="row my-3">
+            <div className="col-12">
+              <h4 className="fs-3 fw-bold">{selectedLanguageData?.my_account}</h4>
+            </div>
+          </div>
+          <Tab.Container id="left-tabs-example" defaultActiveKey="personalDetails">
+            <Row className="">
+              <Col lg={3} className="mb-2">
+                <Nav variant="" className="flex-column" style={{ position: 'sticky', top: '64px', bottom: '0' }}>
+                  <Nav.Item className="border">
+                    <Nav.Link eventKey="personalDetails" className="fs-5 d-flex justify-content-center flex-column align-items-center">
+                      <div>
+                        <FaUser />
+                      </div>
+                      <div>{selectedLanguageData?.profile}</div>
+                    </Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item className="border">
+                    <Nav.Link eventKey="enquiryHistory" className="fs-5 d-flex justify-content-center flex-column align-items-center">
+                      <div>
+                        <FaCalendarDays />
+                      </div>
+                      <div>{selectedLanguageData?.enquiry_history}</div>
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col lg={9}>
+                <Tab.Content>
+                  <Tab.Pane eventKey="personalDetails">
+                    <div className="">
+                      <div className="row">
+                        <div className="col-md-8">
+                          <div className="card h-100">
+                            <div className="row m-2">
+                              <div className="col-12">
+                                <h4 className="fw-bolder">{selectedLanguageData?.personal_details}</h4>
+                              </div>
+                              {arrForPersonalDetails.map((detail, index) => (
+                                <>
+                                  <div className="col-4 mb-1 fw-bolder" key={index}>
+                                    <div>{detail.label}</div>
+                                  </div>
+                                  <div className="col-1"></div>
+                                  <div className="col-7">
+                                    <div>{detail.value}</div>
+                                  </div>
+                                </>
+                              ))}
+                            </div>
+                          </div>
                         </div>
-                        <div className="col-md-6 ">Name:</div>
-                        <div className="col-md-6 fw-bold">Karan Tamboli</div>
+                        <div className="col-md-4">
+                          <div className="card h-100">
+                            <div className="row m-2">
+                              <div className="col-12">
+                                <h4 className="fw-bolder">{selectedLanguageData?.coupons}</h4>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="col-md-6">
+                    <div className=" my-3">
+                      <div className="row">
+                        {arrForAddressDetails.map((address, index) => (
+                          <div className="col-md-6" key={index}>
+                            <div className="row mx-1 border">
+                              <div className="col-12">
+                                <h4 className="fw-bold my-2">{address.label}</h4>
+                              </div>
+                              {address.value.map((detail, i) => (
+                                <React.Fragment key={i}>
+                                  <div className="col-6 mb-2">{detail.label}:</div>
+                                  <div className="col-6 fw-bold ">{detail.value}</div>
+                                </React.Fragment>
+                              ))}
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="address"></Tab.Pane>
+                  <Tab.Pane eventKey="offers"></Tab.Pane>
+                  <Tab.Pane eventKey="payment">Second tab content</Tab.Pane>
+                  <Tab.Pane eventKey="enquiryHistory">
+                    <div className="card">
                       <div className="row">
                         <div className="col-12">
-                          <h4 className="fw-bold">Shipping Address</h4>
+                          <h4>Enquiry History</h4>
                         </div>
-                        <div className="col-md-6">Name:</div>
-                        <div className="col-md-6 fw-bold">Karan Tamboli</div>
                       </div>
+                      <EnquiryHistory />
                     </div>
-                  </div>
-                </div>
-              </Tab.Pane>
-              <Tab.Pane eventKey="address"></Tab.Pane>
-              <Tab.Pane eventKey="offers"></Tab.Pane>
-              <Tab.Pane eventKey="payment">Second tab content</Tab.Pane>
-              <Tab.Pane eventKey="enquiryHistory">
-                <div className="card">
-                  <Table striped>
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                      </tr>
-                      <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </div>
-              </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
-    </div>
-  );
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
+        </div>
+      </div>
+    );
+  }
+  if (errorMessage) {
+    return <div>{errorMessage}</div>;
+  }
+  return <></>;
 };
 
 export default ProfileMaster;
