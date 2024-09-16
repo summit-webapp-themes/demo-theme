fb94da9933185c2ae90e3d8fed630c7b386444bd
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-09-16 13:14:57 +0530

    fix: product detail page and wishlist

--- components/ProductPageComponents/CheckStockAvailabilityBtn.tsx
+++ components/ProductPageComponents/CheckStockAvailabilityBtn.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from 'react';
 import styles from '../../styles/components/productDetail.module.scss';
 
+const CheckStockAvailabilityBtn = ({ handleStockAvailabilityData }: any) => {
+  return (
+    <button onClick={() => handleStockAvailabilityData()} className={`border-0 px-5 mx-3 py-2 rounded-1 my-3 ${styles.buttonBackGround}`}>
+      Check Availability
+    </button>
+  );
+};
 
-const CheckStockAvailabilityBtn = ({ handleStockAvailabilityData, quantity }: any) => {
-    console.log(quantity, 'item')
-    return (
-        <button onClick={() => handleStockAvailabilityData(quantity)} className={`border-0 px-5 mx-3 py-2 rounded-1 my-3 ${styles.buttonBackGround}`}>
-            Check Availability
-        </button>
-    )
-}
-
-export default CheckStockAvailabilityBtn
+export default CheckStockAvailabilityBtn;
--- components/ProductPageComponents/StockAvailabilityTable.tsx
+++ components/ProductPageComponents/StockAvailabilityTable.tsx
@@ -1,44 +1,58 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { SelectedFilterLangDataFromStore } from '../../store/slices/general_slices/selected-multilanguage-slice';
 import styles from '../../styles/components/productDetail.module.scss';
 
-
 const StockAvailabilityTable = ({ stockAvailabilityData }: any) => {
-    const [selectedMultiLangData, setSelectedMultiLangData] = useState<any>();
-    const SelectedLangDataFromStore: any = useSelector(SelectedFilterLangDataFromStore);
-    useEffect(() => {
-        if (Object.keys(SelectedLangDataFromStore?.selectedLanguageData)?.length > 0) {
-            setSelectedMultiLangData(SelectedLangDataFromStore?.selectedLanguageData);
-        }
-    }, [SelectedLangDataFromStore]);
-    console.log(stockAvailabilityData, selectedMultiLangData, 'data')
-    return (
-        <>
-            {stockAvailabilityData?.length > 0 && (
-                <>
-                    <div className={`row text-center ${styles.stock_table_header}`}>
-                        <div className="col border py-2">{selectedMultiLangData?.warehouse_name}</div>
-                        <div className="col border py-2">{selectedMultiLangData?.available_stock_qty}</div>
-                        <div className="col border py-2">{selectedMultiLangData?.incoming_stock_qty}</div>
-                        <div className="col-3 border py-2">{selectedMultiLangData?.estimated_incoming_stock_date}</div>
-                        <div className="col border py-2">{selectedMultiLangData?.additional_qty}</div>
-                        <div className="col border py-2">{selectedMultiLangData?.available_on}</div>
-                    </div>
-                    {stockAvailabilityData?.length > 0 && stockAvailabilityData?.map((stock: any) => (
-                        <div className='row text-center'>
-                            <div className="col border h-100 py-2">{stock?.warehouse || '--'}</div>
-                            <div className="col border h-100 py-2">{stock?.qty || '--'}</div>
-                            <div className="col border h-100 py-2">{stock?.incoming_qty || '--'}</div>
-                            <div className="col-3 border h-100 py-2">{stock?.incoming_date || '--'}</div>
-                            <div className="col border h-100 py-2">{stock?.additional_qty || '--'}</div>
-                            <div className="col border h-100 py-2">{stock?.available_on || '--'}</div>
-                        </div>
-                    ))}
-                </>
-            )}
-        </>
-    )
-}
+  const [selectedMultiLangData, setSelectedMultiLangData] = useState<any>();
+  const SelectedLangDataFromStore: any = useSelector(SelectedFilterLangDataFromStore);
+  useEffect(() => {
+    if (Object.keys(SelectedLangDataFromStore?.selectedLanguageData)?.length > 0) {
+      setSelectedMultiLangData(SelectedLangDataFromStore?.selectedLanguageData);
+    }
+  }, [SelectedLangDataFromStore]);
+  return (
+    <>
+      {stockAvailabilityData?.length > 0 && (
+        <table className="table table-bordered table-responsive">
+          <thead>
+            <tr>
+              <th scope="col" className={`${styles.stock_table_header} text-center`}>
+                {selectedMultiLangData?.warehouse_name}
+              </th>
+              <th scope="col" className={`${styles.stock_table_header} text-center`}>
+                {selectedMultiLangData?.available_stock_qty}
+              </th>
+              <th scope="col" className={`${styles.stock_table_header} text-center`}>
+                {selectedMultiLangData?.incoming_stock_qty}
+              </th>
+              <th scope="col" className={`${styles.stock_table_header} text-center`}>
+                {selectedMultiLangData?.estimated_incoming_stock_date}
+              </th>
+              <th scope="col" className={`${styles.stock_table_header} text-center`}>
+                {selectedMultiLangData?.additional_qty}
+              </th>
+              <th scope="col" className={`${styles.stock_table_header} text-center`}>
+                {selectedMultiLangData?.available_on}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {stockAvailabilityData?.map((stock: any) => (
+              <tr>
+                <td className="text-center">{stock?.warehouse || '--'}</td>
+                <td className="text-center">{stock?.qty || '--'}</td>
+                <td className="text-center">{stock?.incoming_qty || '--'}</td>
+                <td className="text-center">{stock?.incoming_date || '--'}</td>
+                <td className="text-center">{stock?.additional_qty || '--'}</td>
+                <td className="text-center">{stock?.available_on || '--'}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
 
-export default StockAvailabilityTable
+export default StockAvailabilityTable;
