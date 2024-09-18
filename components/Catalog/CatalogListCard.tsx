d25ff88030da809d5cd33c9db46a569721cc0eb8
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-09-18 18:13:06 +0530

    fix: review changes fixed

--- components/Catalog/CatalogListCard.tsx
+++ components/Catalog/CatalogListCard.tsx
@@ -6,12 +6,17 @@ const CatalogListCard = ({ catalogList, handleDeleteCatalog, selectedMultiLangDa
   return (
     <>
       <div className="row mt-5 mb-0 mx-auto pb-0 w-100">
-        <div className="offset-lg-2 col-lg-8 p-0 text-center  ">
-          <h4 className="text-captilize pb-2 color-black">
-            <b>{selectedMultiLangData?.catalog_list}</b>
-          </h4>
-
+        <div className="offset-lg-2 col-lg-8 p-0 ">
+          <div className="d-flex justify-content-between">
+            <h4 className="text-captilize pt-2 color-black text-center">
+              <b>{selectedMultiLangData?.catalog_list}</b>
+            </h4>
+            <Link href={`/product-category/?page=1&currency=INR`}>
+              <button className="btn btn-link">{selectedMultiLangData?.add_product}</button>
+            </Link>
+          </div>
           {catalogList?.length > 0 &&
+            catalogList?.reverse() &&
             catalogList?.map((catalog: any, i: any) => (
               <div className="col-md-12 col-lg-12 mt-4 mb-0  " key={i}>
                 <div className="card catalogListing-card rounded-3 mb-5">
@@ -27,7 +32,7 @@ const CatalogListCard = ({ catalogList, handleDeleteCatalog, selectedMultiLangDa
                     </p>
                   </div>
                   <div className="row card-body ">
-                    <div className="col-lg-5 col-md-4 py-2">
+                    <div className="col-lg-6 col-md-6 py-2">
                       <Link href={`/${catalog.url}?page=1&currency=INR`}>
                         <button className="btn btn-outline-primary text-uppercase">
                           {selectedMultiLangData?.view_catalog_product}
@@ -37,7 +42,7 @@ const CatalogListCard = ({ catalogList, handleDeleteCatalog, selectedMultiLangDa
                         </button>
                       </Link>
                     </div>
-                    <div className="col-lg-3 col-md-4 my-lg-0 my-md-0 my-3 py-2 ">
+                    {/* <div className="col-lg-3 col-md-4 my-lg-0 my-md-0 my-3 py-2 ">
                       <Link href={`/product-category/?page=1&currency=INR`}>
                         <button className="btn btn-outline-primary bg-warning bg-opacity-50 text-uppercase">
                           {selectedMultiLangData?.add_product}
@@ -46,12 +51,12 @@ const CatalogListCard = ({ catalogList, handleDeleteCatalog, selectedMultiLangDa
                           </span>
                         </button>
                       </Link>
-                    </div>
-                    <div className="col-lg-4 col-md-4 text-center py-2">
-                      <button className="btn btn-outline-primary mr-lg-5 text-uppercase" onClick={() => handleDeleteCatalog(catalog?.name)}>
+                    </div> */}
+                    <div className="col-lg-6 col-md-6 text-lg-end py-2">
+                      <button className="btn btn-outline-danger mr-lg-5 text-uppercase" onClick={() => handleDeleteCatalog(catalog?.name)}>
                         {selectedMultiLangData?.delete_catalog}
-                        <span className="ps-2 fs-16 mb-1">
-                          <RiDeleteBin2Fill />
+                        <span className="ps-2 fs-16">
+                          <RiDeleteBin2Fill className="mb-1" />
                         </span>
                       </button>
                     </div>
