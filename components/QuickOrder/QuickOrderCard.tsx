c1947195dd544efe54d2ed4f5ce036de8abe36e4
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-09-24 18:24:41 +0530

    fix: quick order fixex

--- components/QuickOrder/QuickOrderCard.tsx
+++ components/QuickOrder/QuickOrderCard.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image';
-import React from 'react';
-import { imageLoader } from '../../utils/image_loader';
-import Link from 'next/link';
-import IndianNumber from './IndianNumber';
-import noDataImg from '../../public/assets/images/no-data.png';
-import { MdDelete } from 'react-icons/md';
 import { Placeholder } from 'react-bootstrap';
+import { MdDelete } from 'react-icons/md';
+import noDataImg from '../../public/assets/images/no-data.png';
+import { imageLoader } from '../../utils/image_loader';
+import IndianNumber from './IndianNumber';
 
 type Props = any;
 
 const QuickOrderCard = (props: Props) => {
   let total: any;
-  const { data, selectedMultiLangData, handleRemove, isLoading } = props;
+  const { data, selectedMultiLangData, removeItemFromQucikList, isLoading, handleQuantityChange } = props;
+  console.log(data, 'data1');
   const showValue = (qty_value: any) => {
+    console.log(qty_value, 'data1');
     if (qty_value === 0) {
       return 1;
     } else {
@@ -48,36 +48,20 @@ const QuickOrderCard = (props: Props) => {
       {data?.map((data: any, index: any) => (
         <>
           <div className="col-lg-2 col-12 d-flex justify-content-center ps-lg-0 products-name cart-image" key={index}>
-            {data.image_url !== null ? (
-              <Image
-                loader={imageLoader}
-                src={`${data.image}`}
-                // src={maximaCard}
-                alt="product-img"
-                width={150}
-                height={150}
-                className="img-fluid p-2"
-              />
+            {data?.image_url !== null ? (
+              <Image loader={imageLoader} src={`${data?.image}`} alt="product-img" width={150} height={150} className="img-fluid p-2" />
             ) : (
-              <Image
-                loader={imageLoader}
-                src={`${noDataImg?.src}`}
-                // src={maximaCard}
-                alt="product-img"
-                width={150}
-                height={150}
-                className="img-fluid p-2"
-              />
+              <Image loader={imageLoader} src={`${noDataImg?.src}`} alt="product-img" width={150} height={150} className="img-fluid p-2" />
             )}
           </div>
           <div className="col-lg-7 col-12 mb-2 color-black fs-14 ">
             <p className="mb-0 quick-detail-lh">
               {' '}
               {selectedMultiLangData?.item_name}
-              {data.item_name}
+              {data?.item_name}
             </p>
             <p className="mb-0 bold d-inline-flex fw-bolder">
-              {selectedMultiLangData?.item_code}:<span>&nbsp;{data.name}</span>
+              {selectedMultiLangData?.item_code}:<span>&nbsp;{data?.name}</span>
             </p>
             <div>
               <p className=" mb-0 quick-detail-lh d-inline-flex">
@@ -85,7 +69,7 @@ const QuickOrderCard = (props: Props) => {
                 <span>
                   {' '}
                   &nbsp;
-                  {data.brand}
+                  {data?.brand}
                 </span>
               </p>
             </div>
@@ -94,7 +78,7 @@ const QuickOrderCard = (props: Props) => {
               <MdDelete
                 className="fs-4 "
                 onClick={() => {
-                  handleRemove(data?.name);
+                  removeItemFromQucikList(data?.name);
                 }}
                 style={{ cursor: 'pointer' }}
               />
@@ -103,31 +87,32 @@ const QuickOrderCard = (props: Props) => {
           </div>
           <div className="col-lg-1 col-12 mx-lg-0 mx-0 price_font_family color-black quick-order-mg">
             <p>
-              {data.price !== 0 ? (
+              {data?.price}
+              {data?.price !== 0 ? (
                 <>
-                  {data.currency_symbol}
+                  {data?.currency_symbol}
                   <IndianNumber value={data?.price} />
                 </>
               ) : (
-                <p className="border price_request">{selectedMultiLangData?.price_on_request}</p>
+                <p className="border ">{selectedMultiLangData?.price_on_request}</p>
               )}
             </p>
           </div>
-          <div className="col-lg-1 col-6 products-name d-inline-flex justify-content-center align-items-start mt-0">
+          <div className="col-lg-1 col-6 mt-0">
             {
               <>
                 <input
-                  className="w-50  text-center mb-3 qt-margins"
+                  className="form-control w-  100 px-0 text-center "
                   value={showValue(data?.min_order_qty)}
-                  // onChange={(e) => handleInputChange(e, index)}
+                  onChange={(e) => handleQuantityChange(data?.item_name, e.target.value)}
                 />
                 <br />
               </>
             }
           </div>
           <div className="col-lg-1 col-12 mx-lg-0 mx-4 price_font_family products-name mb-3 d-inline-flex justify-content-center">
-            {data.currency_symbol}
-            <IndianNumber value={(total = data.price * showValue(data?.min_order_qty))} />
+            {data?.currency_symbol}
+            <IndianNumber value={(total = data?.price * showValue(data?.min_order_qty))} />
           </div>
         </>
       ))}
