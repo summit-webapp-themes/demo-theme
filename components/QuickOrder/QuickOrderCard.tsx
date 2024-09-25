bb71a01823ada6404902e7c3e02cc2607a2242d2
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-09-25 13:56:31 +0530

    fix: quick order changes

--- components/QuickOrder/QuickOrderCard.tsx
+++ components/QuickOrder/QuickOrderCard.tsx
@@ -9,10 +9,8 @@ type Props = any;
 
 const QuickOrderCard = (props: Props) => {
   let total: any;
-  const { data, selectedMultiLangData, removeItemFromQucikList, isLoading, handleQuantityChange } = props;
-  console.log(data, 'data1');
+  const { data, itemList, selectedMultiLangData, removeItemFromQucikList, isLoading, handleQuantityChange } = props;
   const showValue = (qty_value: any) => {
-    console.log(qty_value, 'data1');
     if (qty_value === 0) {
       return 1;
     } else {
@@ -45,77 +43,107 @@ const QuickOrderCard = (props: Props) => {
   }
   return (
     <>
-      {data?.map((data: any, index: any) => (
-        <>
-          <div className="col-lg-2 col-12 d-flex justify-content-center ps-lg-0 products-name cart-image" key={index}>
-            {data?.image_url !== null ? (
-              <Image loader={imageLoader} src={`${data?.image}`} alt="product-img" width={150} height={150} className="img-fluid p-2" />
-            ) : (
-              <Image loader={imageLoader} src={`${noDataImg?.src}`} alt="product-img" width={150} height={150} className="img-fluid p-2" />
-            )}
-          </div>
-          <div className="col-lg-7 col-12 mb-2 color-black fs-14 ">
-            <p className="mb-0 quick-detail-lh">
-              {' '}
-              {selectedMultiLangData?.item_name}
-              {data?.item_name}
-            </p>
-            <p className="mb-0 bold d-inline-flex fw-bolder">
-              {selectedMultiLangData?.item_code}:<span>&nbsp;{data?.name}</span>
-            </p>
-            <div>
-              <p className=" mb-0 quick-detail-lh d-inline-flex">
-                {selectedMultiLangData?.brand}:{' '}
-                <span>
-                  {' '}
-                  &nbsp;
-                  {data?.brand}
-                </span>
+      {data?.map((itemData: any, index: any) => {
+        const localItem = itemList?.find((item: any) => item.item_code === itemData?.name);
+        return (
+          <>
+            <div className="col-md-2 col-12  " key={index}>
+              {itemData?.image_url !== null ? (
+                <Image
+                  loader={imageLoader}
+                  src={`${itemData?.image}`}
+                  alt="product-img"
+                  width={150}
+                  height={150}
+                  className="img-fluid p-2"
+                />
+              ) : (
+                <Image
+                  loader={imageLoader}
+                  src={`${noDataImg?.src}`}
+                  alt="product-img"
+                  width={150}
+                  height={150}
+                  className="img-fluid p-2"
+                />
+              )}
+            </div>
+            <div className="col-md-7 col-12 mb-2 color-black fs-14 ">
+              <p className="mb-0 quick-detail-lh">
+                {' '}
+                {selectedMultiLangData?.item_name}
+                {itemData?.item_name}
+              </p>
+              <p className="mb-0 bold d-inline-flex fw-bolder">
+                {selectedMultiLangData?.item_code}:<span>&nbsp;{itemData?.name}</span>
+              </p>
+              <div>
+                <p className=" mb-0 quick-detail-lh d-inline-flex">
+                  {selectedMultiLangData?.brand}:{' '}
+                  <span>
+                    {' '}
+                    &nbsp;
+                    {itemData?.brand}
+                  </span>
+                </p>
+              </div>
+
+              <div className="text-danger">
+                <MdDelete
+                  className="fs-4 "
+                  onClick={() => {
+                    removeItemFromQucikList(itemData?.name);
+                  }}
+                  style={{ cursor: 'pointer' }}
+                />
+                {selectedMultiLangData?.delete}
+              </div>
+            </div>
+            <div className="col-md-1 col-12 ">
+              <span className="d-block d-md-none">
+                <b>{selectedMultiLangData?.price} :</b>
+              </span>
+              <p>
+                {itemData?.price}
+                {itemData?.price !== 0 ? (
+                  <>
+                    {itemData?.currency_symbol}
+                    <IndianNumber value={itemData?.price} />
+                  </>
+                ) : (
+                  <p className="border ">{selectedMultiLangData?.price_on_request}</p>
+                )}
               </p>
             </div>
-
-            <div className="text-danger">
-              <MdDelete
-                className="fs-4 "
-                onClick={() => {
-                  removeItemFromQucikList(data?.name);
-                }}
-                style={{ cursor: 'pointer' }}
-              />
-              {selectedMultiLangData?.delete}
+            <div className="col-md-1  col-8 mt-0">
+              <div className="d-flex d-md-block">
+                <span className="d-block d-md-none">
+                  <b>{selectedMultiLangData?.quantity_c}:</b>
+                </span>
+                {
+                  <>
+                    <input
+                      className="form-control w-100 mx-2 mx-md-0 text-center px-0"
+                      value={showValue(localItem?.quantity)}
+                      onChange={(e) => handleQuantityChange(itemData?.name, e.target.value)}
+                    />
+                    <br />
+                  </>
+                }
+              </div>
             </div>
-          </div>
-          <div className="col-lg-1 col-12 mx-lg-0 mx-0 price_font_family color-black quick-order-mg">
-            <p>
-              {data?.price}
-              {data?.price !== 0 ? (
-                <>
-                  {data?.currency_symbol}
-                  <IndianNumber value={data?.price} />
-                </>
-              ) : (
-                <p className="border ">{selectedMultiLangData?.price_on_request}</p>
-              )}
-            </p>
-          </div>
-          <div className="col-lg-1 col-6 mt-0">
-            {
-              <>
-                <input
-                  className="form-control w-  100 px-0 text-center "
-                  value={showValue(data?.min_order_qty)}
-                  onChange={(e) => handleQuantityChange(data?.item_name, e.target.value)}
-                />
-                <br />
-              </>
-            }
-          </div>
-          <div className="col-lg-1 col-12 mx-lg-0 mx-4 price_font_family products-name mb-3 d-inline-flex justify-content-center">
-            {data?.currency_symbol}
-            <IndianNumber value={(total = data?.price * showValue(data?.min_order_qty))} />
-          </div>
-        </>
-      ))}
+            <div className="col-md-1 col-6 my-2">
+              <div className="d-flex d-md-block">
+                <span className="d-block d-md-none">
+                  <b>{selectedMultiLangData?.total} :</b>
+                </span>
+                {data?.currency_symbol}
+                <IndianNumber value={(total = 10000 * showValue(localItem?.quantity))} />
+              </div>
+            </div>
+          </>
+        );
+      })}
     </>
   );
 };
