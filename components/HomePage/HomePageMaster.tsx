92aa58ceaa2a0e910527ee4d043c037ef714449b
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-09-27 10:56:33 +0530

    fix: featured collection deleted

--- components/HomePage/HomePageMaster.tsx
+++ components/HomePage/HomePageMaster.tsx
@@ -3,7 +3,6 @@ import TopCategories from './PersonalizedCategories/TopCategories';
 import BrandListing from './BrandSection/BrandListingMaster';
 import CollectionsMaster from './FeaturedCollections/CollectionMaster';
 import style from '../../styles/components/home.module.scss';
-import FeaturedCollection2Master from './FeaturedCollection2/FeaturedCollection2Master';
 
 const HomePageMaster = () => {
   return (
@@ -13,7 +12,6 @@ const HomePageMaster = () => {
         <TopCategories />
         <BrandListing />
         <CollectionsMaster />
-        <FeaturedCollection2Master />
       </div>
     </>
   );
