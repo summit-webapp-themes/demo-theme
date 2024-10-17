676f216dec3e5d84cef9d9aa77cdf75fb8aad12d
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-10-17 12:26:29 +0530

    fix: Loader added

--- components/HomePage/TopArrowCarousel/TopArrowCarouselMaster.tsx
+++ components/HomePage/TopArrowCarousel/TopArrowCarouselMaster.tsx
@@ -1,14 +1,23 @@
+import Image from 'next/image';
 import useHomeTopCategories from '../../../hooks/HomePageHooks/usePersonalizedCategories';
+import ErrorImage from '../../../public/assets/images/error-icon.png';
 import TopArrowSlider from './TopArrrowSlider';
 
 function TopArrowCarouselMaster() {
   const { homeTopCategories, isLoading, errorMessage } = useHomeTopCategories();
-
-  return (
-    <>
-      <TopArrowSlider data={homeTopCategories} />
-    </>
-  );
+  if (isLoading) {
+    return <div>Loading</div>;
+  } else if (homeTopCategories?.length > 0) {
+    return <TopArrowSlider data={homeTopCategories} />;
+  } else if (errorMessage) {
+    return (
+      <div className="p-3 d-flex justify-content-center align-items-center" style={{ fontSize: '40px' }}>
+        <Image src={ErrorImage} width={250} height={250} alt="Error Image" />
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
 
 export default TopArrowCarouselMaster;
