5a227d9d85c780b174a7a78594256f30dfa9dce7
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-10-22 17:20:21 +0530

    fix: top arraoe carousel

--- components/HomePage/HomePageMaster.tsx
+++ components/HomePage/HomePageMaster.tsx
@@ -3,12 +3,10 @@ import HomeBanner from './BannerSection/HomeBanner';
 import BrandListing from './BrandSection/BrandListingMaster';
 import CollectionsMaster from './FeaturedCollections/CollectionMaster';
 import TopCategories from './PersonalizedCategories/TopCategories';
-import TopArrowCarouselMaster from './TopArrowCarousel/TopArrowCarouselMaster';
 
 const HomePageMaster = () => {
   return (
     <>
-      <TopArrowCarouselMaster />
       <HomeBanner />
       <div className={`row ps-lg-5 pe-lg-5 ${style.backgoundColor}`}>
         <TopCategories />
