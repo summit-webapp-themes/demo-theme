5f5e22acf68d254d750dfd224eeda6d6ecc2bed1
amir rai <amir@8848Digital.com>
2024-10-24 19:03:45 +0530

    fix: removed unused code from homPageMaster in home page

--- components/HomePage/HomePageMaster.tsx
+++ components/HomePage/HomePageMaster.tsx
@@ -1,7 +1,6 @@
 import HomeBanner from './BannerSection/HomeBanner';
 import TopCategories from './PersonalizedCategories/TopCategories';
 import BrandListing from './BrandSection/BrandListingMaster';
-import CollectionsMaster from './FeaturedCollections/FeaturedCollectionInspiration1/FeaturedCollectionsInspiration1Master';
 import style from '../../styles/components/home.module.scss';
 import FeaturedCollectionsInspiration1Master from './FeaturedCollections/FeaturedCollectionInspiration1/FeaturedCollectionsInspiration1Master';
 
