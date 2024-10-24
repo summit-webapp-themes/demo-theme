8194fa86ee45a904169a55899bb8cd0233ccba3c
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-10-24 16:25:44 +0530

    fix: code refactor

--- components/HomePage/PersonalizedCategoriesInspiration2/PersonalizedCategoriesSlider.tsx
+++ components/HomePage/PersonalizedCategoriesInspiration2/PersonalizedCategoriesSlider.tsx
@@ -1,6 +1,5 @@
 import { useRef } from 'react';
 import Carousel from 'react-multi-carousel';
-import styles from '../../../styles/components/topCarousel.module.scss';
 import TopArrowCarouselCard from './CategoriesCard';
 
 function PersonalizedCategoriesSlider({ data }: any) {
@@ -13,12 +12,16 @@ function PersonalizedCategoriesSlider({ data }: any) {
       breakpoint: { max: 3000, min: 1200 },
       items: 7,
     },
+    laptop: {
+      breakpoint: { max: 1200, min: 800 },
+      items: 5,
+    },
     tablet: {
-      breakpoint: { max: 1200, min: 600 },
-      items: 2,
+      breakpoint: { max: 800, min: 500 },
+      items: 3,
     },
     mobile: {
-      breakpoint: { max: 600, min: 0 },
+      breakpoint: { max: 500, min: 320 },
       items: 1,
     },
   };
@@ -26,11 +29,11 @@ function PersonalizedCategoriesSlider({ data }: any) {
   const sliderRef = useRef<any>(null);
 
   return (
-    <div className={`${styles.slider_container}`}>
+    <div className={`slider-container`}>
       {/* Custom slider wrapper */}
       <Carousel ref={sliderRef} responsive={responsive} arrows={false} infinite>
         {data?.map((val: any, index: number) => (
-          <div key={index} className={`${styles.slider_item}`}>
+          <div key={index} className={`px-2`}>
             <TopArrowCarouselCard data={val} />
           </div>
         ))}
