9b6dd318baa140f76f3c7bf8f273a2a6316678b9
karan <karan@8848digital.com>
2024-10-24 12:59:24 +0530

    fix: :recycle: home banner inspiration 1 import fixes

--- components/HomePage/HomePageMaster.tsx
+++ components/HomePage/HomePageMaster.tsx
@@ -1,5 +1,4 @@
 import HomeBannersInspiration1Master from './BannerSection/HomeBannersInspiration1/HomeBannersMaster';
-import HomeBannersInspiration3Master from './BannerSection/HomeBannersInspiration3/HomeBannersMaster';
 import TopCategories from './PersonalizedCategories/TopCategories';
 import BrandListing from './BrandSection/BrandListingMaster';
 import CollectionsMaster from './FeaturedCollections/CollectionMaster';
@@ -8,7 +7,7 @@ import style from '../../styles/components/home.module.scss';
 const HomePageMaster = () => {
   return (
     <>
-      <HomeBannersInspiration3Master />
+      <HomeBannersInspiration1Master />
       <div className={`row ps-lg-5 pe-lg-5 ${style.backgoundColor}`}>
         <TopCategories />
         <BrandListing />
