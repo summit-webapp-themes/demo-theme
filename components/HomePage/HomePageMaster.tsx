7ff870580fbfe494e0ec7365c51349ac1a6d2a1d
rushikesh2404 <rushikesh@8848digital.com>
2024-10-24 11:40:40 +0530

    fix: revert homepageMaster changes

--- components/HomePage/HomePageMaster.tsx
+++ components/HomePage/HomePageMaster.tsx
@@ -3,7 +3,6 @@ import TopCategories from './PersonalizedCategories/TopCategories';
 import BrandListing from './BrandSection/BrandListingMaster';
 import CollectionsMaster from './FeaturedCollections/CollectionMaster';
 import style from '../../styles/components/home.module.scss';
-import PersonalizedCategoriesBlocks from './PersonalizedCategories/PersonalizedCategoriesInspiration4/PersonalizedCategoriesBlocks';
 
 const HomePageMaster = () => {
   return (
@@ -11,7 +10,6 @@ const HomePageMaster = () => {
       <HomeBanner />
       <div className={`row ps-lg-5 pe-lg-5 ${style.backgoundColor}`}>
         <TopCategories />
-        <PersonalizedCategoriesBlocks/>
         <BrandListing />
         <CollectionsMaster />
       </div>
