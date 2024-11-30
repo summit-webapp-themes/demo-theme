b3bfff1655ca87f4c47666a8994f2a09bffe4738
karan <karan.tamboli2001@gmai.com>
2024-11-30 19:44:04 +0530

    feat: :sparkles: introduced new product page components to handle Images

---
+++ components/ProductPageComponents/ProductImageGallery/ImageGalleryMaster.tsx
@@ -0,0 +1,15 @@
+import { ProductSlideshowImages } from '../../../interfaces/product-slideshow-images';
+import ImageGalleryWithLeftThumbnails from './ImageGalleryWithLeftThumbnails';
+import ImageGalleryWithRightThumbnails from './ImageGalleryWithRightThumbnails';
+import ImageGalleryWithBottomThumbnails from './ImageGalleryWithBottomThumbnails';
+const ImageGalleryMaster = ({ slideShowImages }: ProductSlideshowImages) => {
+  return (
+    <>
+      {/* <ImageGalleryWithLeftThumbnails slideShowImages={slideShowImages} /> */}
+      {/* <ImageGalleryWithRightThumbnails slideShowImages={slideShowImages} /> */}
+      {/* <ImageGalleryWithBottomThumbnails slideShowImages={slideShowImages} /> */}
+    </>
+  );
+};
+
+export default ImageGalleryMaster;
