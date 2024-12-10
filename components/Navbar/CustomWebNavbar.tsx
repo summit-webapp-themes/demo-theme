410d562946f1611d0418dbbc8402b9ad18ac453f
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-12-10 12:59:47 +0530

    fix: navbar css fixes

--- components/Navbar/CustomWebNavbar.tsx
+++ components/Navbar/CustomWebNavbar.tsx
@@ -3,14 +3,13 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { NavDropdown } from 'react-bootstrap';
-import { FaSearch, FaUserCircle } from 'react-icons/fa';
-import { FaAlignJustify, FaCartPlus, FaHeart } from 'react-icons/fa6';
+import { FaUserCircle } from 'react-icons/fa';
+import { FiHeart, FiShoppingCart, FiUser } from 'react-icons/fi';
+import { RiMenu2Line } from 'react-icons/ri';
 import logo from '../../public/assets/images/logo.png';
 import stylesNavbar from '../../styles/components/navbar.module.scss';
-import MobileProductCategories from './MobileProductCategories';
-import ProductCatagoriesNavbar from './ProductCatagoriesNavbar';
 import CustomProductCategoriesNavbar from './CustomProductCategoriesNavbar';
-import { FiHeart, FiShoppingCart, FiUser } from 'react-icons/fi';
+import MobileProductCategories from './MobileProductCategories';
 
 function CustomWebNavbar({
   navbarData,
@@ -53,7 +52,7 @@ function CustomWebNavbar({
       <header className={stylesNavbar.header}>
         <nav>
           <div className={`${stylesNavbar.navbar} ps-lg-4 pe-lg-4`}>
-            <div className="w-100 d-flex justify-content-start py-2 align-items-center">
+            <div className="w-100 d-flex justify-content-between py-2 align-items-center">
               <div className="mobile-nav d-flex justify-content-sm-between px-3 px-sm-4 d-sm-block d-md-none">
                 <Link href="#" legacyBehavior>
                   <a
@@ -61,140 +60,138 @@ function CustomWebNavbar({
                     aria-label="menu-toggle"
                     onClick={() => setIsSidebarOpen(!isSidebarOpen)}
                   >
-                    <FaAlignJustify className="icon" />
+                    <RiMenu2Line className="icon" />
                   </a>
                 </Link>
               </div>
-              <div className="d-flex justify-content-between align-items-center w-100">
-                <div>
-                  <Link href="/" legacyBehavior>
-                    <a>
-                      <Image className=" " src={logo} alt="logo" width={250} priority={true} />
-                    </a>
-                  </Link>
-                </div>
+              {/* <div className="d-flex justify-content-between align-items-center w-100"> */}
+              <div>
+                <Link href="/" legacyBehavior>
+                  <a>
+                    <Image className=" " src={logo} alt="logo" width={250} priority={true} />
+                  </a>
+                </Link>
+              </div>
 
-                {/* Product Categories Navbar */}
-                <div className="w-50 d-none d-sm-none d-md-block">
-                  <CustomProductCategoriesNavbar
-                    navbarData={navbarData}
-                    errorMessage={errorMessage}
-                    selectedCurrencyValue={selectedCurrencyValue}
-                    multiLanguagesData={multiLanguagesData}
-                    selectedLang={selectedLang}
-                    handleLanguageChange={handleLanguageChange}
-                  />
-                </div>
+              {/* Product Categories Navbar */}
+              <div className="w-50 d-none d-sm-none d-md-block">
+                <CustomProductCategoriesNavbar
+                  navbarData={navbarData}
+                  errorMessage={errorMessage}
+                  selectedCurrencyValue={selectedCurrencyValue}
+                  multiLanguagesData={multiLanguagesData}
+                  selectedLang={selectedLang}
+                  handleLanguageChange={handleLanguageChange}
+                />
+              </div>
 
-                <div className={` ${stylesNavbar.inlineList}`}>
-                  <ul className="nav list-inline d-flex justify-content-end align-items-center">
-                    <li className={`${stylesNavbar.list_inline_item_custom} `}>
-                      {isLoggedIn ? (
-                        <>
-                          <div className={stylesNavbar.icon_container} onClick={handleShowDropDown}>
-                            <FaUserCircle className="icon" />
-                            <span className={`d-none d-md-inline-block theme-blue ${stylesNavbar.order_list_dropdown}`}>{user}</span>
-                          </div>
+              <div className={``}>
+                <ul className=" list-inline d-flex justify-content-end align-items-center m-0">
+                  <li className={`${stylesNavbar.list_inline_item_custom} `}>
+                    {isLoggedIn ? (
+                      <>
+                        <div className={stylesNavbar.icon_container} onClick={handleShowDropDown}>
+                          <FaUserCircle className="icon" />
+                          <span className={`d-none d-md-inline-block theme-blue ${stylesNavbar.order_list_dropdown}`}>{user}</span>
+                        </div>
 
-                          {isLoggedIn && (
-                            <NavDropdown
-                              title={''}
-                              id="basic-nav-dropdown"
-                              className={` ${stylesNavbar.order_list_dropdown}`}
-                              show={showDropDown}
-                              onToggle={handleToggle}
-                            >
+                        {isLoggedIn && (
+                          <NavDropdown
+                            title={''}
+                            id="basic-nav-dropdown"
+                            className={` ${stylesNavbar.order_list_dropdown}`}
+                            show={showDropDown}
+                            onToggle={handleToggle}
+                          >
+                            <NavDropdown.Item
+                              as="a"
+                              className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
+                            ></NavDropdown.Item>
+
+                            <Link href="/quick-order" passHref className="text-decoration-none">
                               <NavDropdown.Item
                                 as="a"
                                 className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
-                              ></NavDropdown.Item>
+                              >
+                                {selectedLanguageData?.quick_order}
+                              </NavDropdown.Item>
+                            </Link>
+                            <Link href="/quick-order" passHref className="text-decoration-none">
+                              <NavDropdown.Item
+                                as="a"
+                                className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
+                              >
+                                {selectedLanguageData?.my_account}
+                              </NavDropdown.Item>
+                            </Link>
+                            <Link href="/quick-order" passHref className="text-decoration-none">
+                              <NavDropdown.Item
+                                as="a"
+                                className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
+                              >
+                                {selectedLanguageData?.dealer_ledger}
+                              </NavDropdown.Item>
+                            </Link>
+                            <Link href="/catalog" passHref className="text-decoration-none">
+                              <NavDropdown.Item
+                                as="a"
+                                className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
+                              >
+                                {selectedLanguageData?.view_catalogs}
+                              </NavDropdown.Item>
+                            </Link>
+                            <Link href="/my-orders" passHref className="text-decoration-none">
+                              <NavDropdown.Item
+                                as="a"
+                                className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
+                              >
+                                {selectedLanguageData?.my_order}
+                              </NavDropdown.Item>
+                            </Link>
 
-                              <Link href="/quick-order" passHref className="text-decoration-none">
-                                <NavDropdown.Item
-                                  as="a"
-                                  className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
-                                >
-                                  {selectedLanguageData?.quick_order}
-                                </NavDropdown.Item>
-                              </Link>
-                              <Link href="/quick-order" passHref className="text-decoration-none">
-                                <NavDropdown.Item
-                                  as="a"
-                                  className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
-                                >
-                                  {selectedLanguageData?.my_account}
-                                </NavDropdown.Item>
-                              </Link>
-                              <Link href="/quick-order" passHref className="text-decoration-none">
-                                <NavDropdown.Item
-                                  as="a"
-                                  className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
-                                >
-                                  {selectedLanguageData?.dealer_ledger}
-                                </NavDropdown.Item>
-                              </Link>
-                              <Link href="/catalog" passHref className="text-decoration-none">
-                                <NavDropdown.Item
-                                  as="a"
-                                  className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
-                                >
-                                  {selectedLanguageData?.view_catalogs}
-                                </NavDropdown.Item>
-                              </Link>
-                              <Link href="/my-orders" passHref className="text-decoration-none">
-                                <NavDropdown.Item
-                                  as="a"
-                                  className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
-                                >
-                                  {selectedLanguageData?.my_order}
-                                </NavDropdown.Item>
-                              </Link>
-
-                              <Link href="#" passHref className="text-decoration-none" onClick={handleLogoutUser}>
-                                <NavDropdown.Item
-                                  as="a"
-                                  className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
-                                >
-                                  {selectedLanguageData?.logout}
-                                </NavDropdown.Item>
-                              </Link>
-                            </NavDropdown>
-                          )}
-                        </>
-                      ) : (
-                        <Link href={'/login'} className={stylesNavbar.icon_container} onClick={handleShowDropDown}>
-                          <FiUser className="icon" />
-                        </Link>
-                      )}
-                    </li>
-                    <li className={` ${stylesNavbar.list_inline_item_custom} `}>
-                      <Link href="/wishlist " legacyBehavior>
-                        <a className={`link-dark ${stylesNavbar.label}`}>
-                          <div className={stylesNavbar.icon_container}>
-                            <FiHeart className="icon" />
-                            <span className={`${stylesNavbar.badge} ${stylesNavbar.badge_warning} ${stylesNavbar.badge_custom} text-white`}>
-                              {wishlistCount}
-                            </span>
-                          </div>
-                        </a>
+                            <Link href="#" passHref className="text-decoration-none" onClick={handleLogoutUser}>
+                              <NavDropdown.Item
+                                as="a"
+                                className={`text-decoration-none ${stylesNavbar.order_list_items} custom-dropdown-item`}
+                              >
+                                {selectedLanguageData?.logout}
+                              </NavDropdown.Item>
+                            </Link>
+                          </NavDropdown>
+                        )}
+                      </>
+                    ) : (
+                      <Link href={'/login'} className={stylesNavbar.icon_container} onClick={handleShowDropDown}>
+                        <FiUser className="icon" />
                       </Link>
-                    </li>
-                    <li className={`${stylesNavbar.list_inline_item_custom} ${stylesNavbar.cart_icon_mob}`}>
-                      <Link href="/cart" legacyBehavior>
-                        <a className={`link-dark ${stylesNavbar.label}`}>
-                          <div className={stylesNavbar.icon_container}>
-                            <FiShoppingCart className="icon" />
-                            <span
-                              className={`${stylesNavbar.badge} ${stylesNavbar.badge_warning}  ${stylesNavbar.badge_custom} text-white`}
-                            >
-                              {cartCount}
-                            </span>
-                          </div>
-                        </a>
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
+                    )}
+                  </li>
+                  <li className={` ${stylesNavbar.list_inline_item_custom} `}>
+                    <Link href="/wishlist " legacyBehavior>
+                      <a className={`link-dark ${stylesNavbar.label}`}>
+                        <div className={stylesNavbar.icon_container}>
+                          <FiHeart className="icon" />
+                          <span className={`${stylesNavbar.badge} ${stylesNavbar.badge_warning} ${stylesNavbar.badge_custom} text-white`}>
+                            {wishlistCount}
+                          </span>
+                        </div>
+                      </a>
+                    </Link>
+                  </li>
+                  <li className={`${stylesNavbar.list_inline_item_custom} ${stylesNavbar.cart_icon_mob}`}>
+                    <Link href="/cart" legacyBehavior>
+                      <a className={`link-dark ${stylesNavbar.label}`}>
+                        <div className={stylesNavbar.icon_container}>
+                          <FiShoppingCart className="icon" />
+                          <span className={`${stylesNavbar.badge} ${stylesNavbar.badge_warning}  ${stylesNavbar.badge_custom} text-white`}>
+                            {cartCount}
+                          </span>
+                        </div>
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+                {/* </div> */}
               </div>
             </div>
           </div>
