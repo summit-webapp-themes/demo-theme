4890ce7179e89626a831e326bac59138d408f993
karan <karan.tamboli2001@gmail.com>
2024-12-11 13:58:42 +0530

    fix: :bug: added new provider file to test stoe

--- _app.tsx
+++ _app.tsx
@@ -10,6 +10,7 @@ import { persistor, store } from '../store/store';
 import useInitializeGoogleAnalytics from '../hooks/GoogleAnalytics/useInitializeGoogleAnalytics';
 import ErrorBoundary from '../components/ErrorBoundary';
 const Layout = dynamic(() => import('../components/Layout'));
+import StoreDataProvider from '../components/StoreDataProvider';
 const ProtectedRoute = dynamic(() => import('../routes/ProtectedRoute'));
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -33,28 +34,30 @@ function MyApp({ Component, pageProps }: AppProps) {
     <div className={nunito.className}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <ErrorBoundary>
-            <Layout>
-              <ToastContainer
-                position="top-right"
-                className="toast-container-below-navbar"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                draggable={false}
-                closeOnClick
-                pauseOnHover
-              />
-              {/* Below condition is to check whether give complete access of site to guest user or user can access site only after authentication */}
-              {CONSTANTS.ALLOW_GUEST_TO_ACCESS_SITE_EVEN_WITHOUT_AUTHENTICATION ? (
-                <Component {...pageProps} />
-              ) : (
-                <ProtectedRoute>
+          <StoreDataProvider>
+            <ErrorBoundary>
+              <Layout>
+                <ToastContainer
+                  position="top-right"
+                  className="toast-container-below-navbar"
+                  autoClose={3000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  draggable={false}
+                  closeOnClick
+                  pauseOnHover
+                />
+                {/* Below condition is to check whether give complete access of site to guest user or user can access site only after authentication */}
+                {CONSTANTS.ALLOW_GUEST_TO_ACCESS_SITE_EVEN_WITHOUT_AUTHENTICATION ? (
                   <Component {...pageProps} />
-                </ProtectedRoute>
-              )}
-            </Layout>
-          </ErrorBoundary>
+                ) : (
+                  <ProtectedRoute>
+                    <Component {...pageProps} />
+                  </ProtectedRoute>
+                )}
+              </Layout>
+            </ErrorBoundary>
+          </StoreDataProvider>
         </PersistGate>
       </Provider>
     </div>
