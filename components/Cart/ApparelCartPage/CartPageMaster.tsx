7b4c2bed33dd143274f96800d0abb035d5d358ae
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-12-17 11:40:38 +0530

    feat: cart page master component added

---
+++ components/Cart/ApparelCartPage/CartPageMaster.tsx
@@ -0,0 +1,31 @@
+import useAddToCartHook from '../../../hooks/CartPageHook/useAddToCart';
+import useFetchCartItems from '../../../hooks/CartPageHook/useFetchCartItems';
+import NoDataFound from '../../NoRecordFound';
+
+const CartPageMaster = () => {
+  const { cartListingItems, setCartListingItems, isLoading, errorMessage } = useFetchCartItems();
+  const { addToCartItem, cLearCartAPIFunc, RemoveItemCartAPIFunc }: any = useAddToCartHook();
+
+  const message = `
+    Before proceeding to checkout, you must add some products to your shopping cart.
+    You will find a lot of interesting products on our Home page.
+  `;
+  const handleDataRendering = () => {
+    if (isLoading) {
+      return (
+        <div className="vh-100 d-flex justify-content-center align-items-center">
+          <div className="spinner-border" role="status" />
+        </div>
+      );
+    }
+    if (!isLoading && Object.keys(cartListingItems)?.length !== 0) {
+      return <div>Cart List</div>;
+    }
+    if (!isLoading && Object.keys(cartListingItems)?.length === 0) {
+      return <NoDataFound title={'YOUR CART IS EMPTY.'} const message={message} />;
+    }
+  };
+  return <>{handleDataRendering()}</>;
+};
+
+export default CartPageMaster;
