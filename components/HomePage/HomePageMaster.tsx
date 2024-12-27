23b07e6d545e8be79c0bfdea48602c45f723408a
karan <karan.tamboli2001@gmai.com>
2024-12-27 13:18:14 +0530

    feat: :zap: calling banner component on build to reduce LCP score

--- components/HomePage/HomePageMaster.tsx
+++ components/HomePage/HomePageMaster.tsx
@@ -1,18 +1,20 @@
 import flattenComponentsList from '../../utils/handle-components-list';
 
-const HomePageMaster = ({ homePageComponents }: any) => {
+const HomePageMaster = ({ homePageComponents, bannerData }: any) => {
   const componentsListFlattenArray = flattenComponentsList(homePageComponents);
   if (homePageComponents?.length === 0) {
     return <p>No components to display for the home page.</p>;
   }
   if (componentsListFlattenArray?.length === 0) return <p>No components to display for the home page.</p>;
 
-  if (componentsListFlattenArray?.length > 0) {
-    console.log('componentsListFlattenArray', componentsListFlattenArray);
-    return componentsListFlattenArray?.map((componentName: any) => {
-      const Component = require(`./${componentName.section_name}/${componentName?.component_name}/MasterComponent`).default;
-      return <Component key={componentName?.component_name} />;
-    });
-  }
+  const componentsToRender = componentsListFlattenArray?.map((componentName: any) => {
+    const Component = require(`./${componentName.section_name}/${componentName?.component_name}/MasterComponent`).default;
+    if (componentName?.section_name === 'BannerSection') {
+      return <Component key={componentName?.component_name} bannerData={bannerData} />;
+    }
+    return <Component key={componentName?.component_name} />;
+  });
+
+  return <>{componentsToRender}</>;
 };
 export default HomePageMaster;
