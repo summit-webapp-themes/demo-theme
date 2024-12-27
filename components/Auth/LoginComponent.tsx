4dad6e132e93acb548757c7c4e72d010929a8371
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-12-27 18:19:32 +0530

    fix: formik removed

--- components/Auth/LoginComponent.tsx
+++ components/Auth/LoginComponent.tsx
@@ -1,113 +1,118 @@
-import { ErrorMessage, Formik, Form as FormikForm } from 'formik';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { InputGroup } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import * as Yup from 'yup';
 import useLoginHook from '../../hooks/AuthHooks/useLoginHook';
 import logo from '../../public/assets/images/progearhub.png';
 import LoginStyles from '../../styles/components/login.module.scss';
 
-const validation = Yup.object().shape({
-  usr: Yup.string().email(' Enter valid email').required(' Email field is required'),
-  pwd: Yup.string().required(' Password field is required').min(6, ' Password is too short - should be 6 chars minimum.'),
-});
-
 function LoginComponent() {
   const { passwordHidden, togglePasswordIcon, fetchToken, loginBtnLoader } = useLoginHook();
-  return (
-    <Formik
-      initialValues={{
-        usr: '',
-        pwd: '',
-      }}
-      validationSchema={validation}
-      onSubmit={(values: any) => {
-        fetchToken(values);
-      }}
-    >
-      {({ handleChange, handleBlur, values }) => (
-        <FormikForm>
-          <div className="form-wrapper" id="wrapper-login">
-            <div className="content-wrapper" id="content-signin">
-              <div className="row mx-4 mx-sm-0">
-                <div className={`offset-lg-4 offset-md-3 offset-sm-2 col-sm-8 col-12  col-lg-4 col-md-6 ${LoginStyles.main_column}`}>
-                  <div className="row px-4">
-                    <div className="col-12">
-                      <div className="img">
-                        <Link href="/" className="navbar-brand">
-                          <Image src={logo} alt="logo" width={160} className="img-fluid mx-auto d-block mb-4 h-auto" priority={true} />
-                        </Link>
-                      </div>
-                      <Form.Group controlId="formEmail">
-                        <Form.Label>Email ID </Form.Label>
-                        <Form.Control
-                          onChange={handleChange}
-                          type="text"
-                          name="usr"
-                          placeholder="Enter Email"
-                          className={`${LoginStyles.emailfield}`}
-                          onBlur={handleBlur}
-                        />
-                        <div className={`${LoginStyles.empty} mt-1`}>
-                          <ErrorMessage name="usr" />
-                        </div>
-                      </Form.Group>
-                    </div>
-                  </div>
-                  <div className="row px-4 mt-1 ">
-                    <div className="col-12 mt-2">
-                      <Form.Group controlId="formPassword">
-                        <Form.Label>Password </Form.Label>
-                        <InputGroup>
-                          <Form.Control
-                            onChange={handleChange}
-                            type={passwordHidden ? 'password' : 'text'}
-                            name="pwd"
-                            placeholder="Enter Password"
-                            onBlur={handleBlur}
-                          />
-                          <InputGroup.Text id="basic-addon2">
-                            <span className={``}>
-                              {passwordHidden ? (
-                                <FaEyeSlash onClick={(e: any) => togglePasswordIcon(e)} />
-                              ) : (
-                                <FaEye onClick={(e: any) => togglePasswordIcon(e)} />
-                              )}
-                            </span>
-                          </InputGroup.Text>
-                        </InputGroup>
-                        <div className={`${LoginStyles.empty} mt-1`}>
-                          <ErrorMessage name="pwd" />
-                        </div>
-                      </Form.Group>
-                    </div>
-                  </div>
-                  <div className="row px-4 mt-3">
-                    <div className="col-12 d-flex justify-content-center">
-                      <button type="submit" className={`${LoginStyles.btn_login} rounded w-100 mb-3 mt-2`}>
-                        {loginBtnLoader ? (
-                          <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                        ) : (
-                          <span>LOGIN</span>
-                        )}
-                      </button>
-                    </div>
-                  </div>
+  const [formData, setFormData] = useState({ usr: '', pwd: '' });
+  const [errors, setErrors] = useState({ usr: '', pwd: '' });
 
-                  <div className="text-center">
-                    <span className="not_an_account">
-                      <span className="color-black">New to Summit?</span> <Link href="/register">Create an account</Link>
-                    </span>
+  const validate = () => {
+    const newErrors = { usr: '', pwd: '' };
+
+    if (!formData.usr) {
+      newErrors.usr = 'Email field is required';
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.usr)) {
+      newErrors.usr = 'Enter a valid email';
+    }
+
+    if (!formData.pwd) {
+      newErrors.pwd = 'Password field is required';
+    } else if (formData.pwd.length < 6) {
+      newErrors.pwd = 'Password is too short - should be 6 chars minimum';
+    }
+
+    setErrors(newErrors);
+    return !newErrors.usr && !newErrors.pwd;
+  };
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (validate()) {
+      fetchToken(formData);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-wrapper" id="wrapper-login">
+        <div className="content-wrapper" id="content-signin">
+          <div className="row mx-4 mx-sm-0">
+            <div className={`offset-lg-4 offset-md-3 offset-sm-2 col-sm-8 col-12 col-lg-4 col-md-6 ${LoginStyles.main_column}`}>
+              <div className="row px-4">
+                <div className="col-12">
+                  <div className="img">
+                    <Link href="/" className="navbar-brand">
+                      <Image src={logo} alt="logo" width={160} className="img-fluid mx-auto d-block mb-4 h-auto" priority={true} />
+                    </Link>
                   </div>
+                  <Form.Group controlId="formEmail">
+                    <Form.Label>Email ID</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="usr"
+                      placeholder="Enter Email"
+                      value={formData.usr}
+                      onChange={handleChange}
+                      className={`${LoginStyles.emailfield}`}
+                    />
+                    {errors.usr && <div className={`${LoginStyles.empty} mt-1`}>{errors.usr}</div>}
+                  </Form.Group>
                 </div>
+              </div>
+              <div className="row px-4 mt-1">
+                <div className="col-12 mt-2">
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <InputGroup>
+                      <Form.Control
+                        type={passwordHidden ? 'password' : 'text'}
+                        name="pwd"
+                        placeholder="Enter Password"
+                        value={formData.pwd}
+                        onChange={handleChange}
+                      />
+                      <InputGroup.Text id="basic-addon2">
+                        <span>{passwordHidden ? <FaEyeSlash onClick={togglePasswordIcon} /> : <FaEye onClick={togglePasswordIcon} />}</span>
+                      </InputGroup.Text>
+                    </InputGroup>
+                    {errors.pwd && <div className={`${LoginStyles.empty} mt-1`}>{errors.pwd}</div>}
+                  </Form.Group>
+                </div>
+              </div>
+              <div className="row px-4 mt-3">
+                <div className="col-12 d-flex justify-content-center">
+                  <button type="submit" className={`${LoginStyles.btn_login} rounded w-100 mb-3 mt-2`} disabled={loginBtnLoader}>
+                    {loginBtnLoader ? (
+                      <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    ) : (
+                      <span>LOGIN</span>
+                    )}
+                  </button>
+                </div>
+              </div>
+
+              <div className="text-center">
+                <span className="not_an_account">
+                  <span className="color-black">New to Summit?</span> <Link href="/register">Create an account</Link>
+                </span>
               </div>
             </div>
           </div>
-        </FormikForm>
-      )}
-    </Formik>
+        </div>
+      </div>
+    </form>
   );
 }
 
