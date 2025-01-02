b7094c6837f127b79ebfb04d4466046b03334e9c
rushikesh2404 <rushikesh@8848digital.com>
2025-01-02 18:26:02 +0530

    feat: add one more banner section

---
+++ components/HomePage/BannerSection/HomeBannerWithLeftAndCenterAlignContent/MasterComponent.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+import HomeBanners from './HomeBanners';
+import HomeBannerLoading from './HomeBannersLoading';
+import ErrorImage from '../../../../public/assets/images/error-icon.png';
+
+const MasterComponent = ({ bannerData }: any) => {
+  console.log('center ', bannerData);
+  if (bannerData?.data?.length > 0) {
+    return <HomeBanners bannersList={bannerData?.data} />;
+  }
+  if (bannerData?.data?.length === 0) {
+    return (
+      <div className="p-3 d-flex justify-content-center align-items-center" style={{ fontSize: '40px' }}>
+        <Image src={ErrorImage} width={250} height={250} alt="Error Image" />
+      </div>
+    );
+  }
+  return <HomeBannerLoading />;
+};
+
+export default MasterComponent;
