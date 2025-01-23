bf7c84bcaffad14813fa28c2fb9864c1fab736e2
NikitaD8848 <nikita.dhumal@8848digital.com>
2025-01-23 18:06:29 +0530

    feat: promotional banner with btn added

---
+++ components/HomePage/PromotionalBanners/PromotionalBannerWithBtn/PromotionalBannerWithBtnLoader.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Placeholder } from 'react-bootstrap';
+
+function PromotionalBannerWithBtnLoader() {
+  return (
+    <div className="custom-container-xl px-4 mt-5">
+      <div className="row">
+        {[...Array(2)].map((_, index) => (
+          <div key={index} className="col-md-6 col-12 rounded ">
+            <Placeholder animation="glow">
+              <Placeholder style={{ width: '100%', height: 350 }} />
+            </Placeholder>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default PromotionalBannerWithBtnLoader;
