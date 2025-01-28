40141fd0184db7eebe87122ca689a4bddb109f07
monika8848 <monika@8848digital.com>
2025-01-28 18:05:28 +0530

    fix: ui fixes of categories promotional banner of mobile view

--- components/HomePage/PromotionalBanners/PromotionalBanners1/PromotionalBanners1CardContainer.tsx
+++ components/HomePage/PromotionalBanners/PromotionalBanners1/PromotionalBanners1CardContainer.tsx
@@ -3,11 +3,11 @@ import PromotionalBanners1Card from './PromotionalBanners1Card';
 
 function PromotionalBanners1CardContainer({ promotionalBannerData }: any) {
   return (
-    <div className="custom-container-xl pt-5 h-100">
+    <div className="custom-container-xl pt-2 h-100">
       <div className="py-5">
         <div className="row">
           {promotionalBannerData.map((item: any, index: any) => (
-            <div className="col-6 px-3" key={index}>
+            <div className="col-md-6 px-3 mt-3" key={index}>
               <PromotionalBanners1Card data={item} />
             </div>
           ))}
