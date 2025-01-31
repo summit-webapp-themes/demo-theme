dcafb7c148089224feca3078cdf210ad0f3aa54b
monika8848 <monika@8848digital.com>
2025-01-31 12:44:14 +0530

    feat: changed ui of error page

--- components/PageNotFound.tsx
+++ components/PageNotFound.tsx
@@ -9,33 +9,10 @@ function PageNotFound() {
 
   return (
     <>
-      {/* <div className="d-flex justify-content-center" style={{ height: '80vh', alignItems: 'center' }}>
-        <div className="row">
-          <div className="col-md-6"></div>
-        </div>
-        <div className={`${pageNotFoundStyles.error_content}`}>
-          <div className="p-3" style={{ fontSize: '40px' }}>
-            <Image src={image} width={376} height={461} alt="Error Image" />
-          </div>
-          <h4>Page not Found!</h4>
-          <p className="grey">
-            Sorry, We're having trouble processing your request right now.
-            <br />
-            Please try again later.
-          </p>
-          <button className={`btn btn-primary ${pageNotFoundStyles.error_button} `} onClick={() => router.push('/')}>
-            Back to Home
-          </button>
-        </div>
-      </div> */}
-
-      <div className="d-flex justify-content-center align-items-center" style={{ height: '90vh' }}>
-        <div
-          className="container d-flex flex-column flex-md-row align-items-center justify-content-center text-center text-md-start"
-          // style={{ maxWidth: '1100px' }}
-        >
+      <div className={`d-flex justify-content-center align-items-center ${pageNotFoundStyles.page_not_found_main_container}`}>
+        <div className="container d-flex flex-column flex-lg-row align-items-center justify-content-center text-center text-md-start">
           {/* Error Image (Left - Centered) */}
-          <div className={`d-flex justify-content-end pe-5  ${pageNotFoundStyles.error_img_container}`} style={{ flex: 1 }}>
+          <div className={`d-flex justify-content-end  ${pageNotFoundStyles.error_img_container}`} style={{ flex: 1 }}>
             <Image src={image} width={376} height={461} alt="Error Image" />
           </div>
 
