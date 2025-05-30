0758a4cd97511f7b4a00708c4baf2d0303ac7bf3
Navneet <navneet@8848digital.com>
2025-05-30 15:30:34 +0530

    feat: uncomment code in default layout

--- components/ProductCategoriesComponents/ProductListPageLayout/DefaultLayout.tsx
+++ components/ProductCategoriesComponents/ProductListPageLayout/DefaultLayout.tsx
@@ -51,10 +51,10 @@ const DefaultLayout: FC<DefaultLayoutProps> = ({ filterComponent, CardsComponent
   } else if (filterComponent === 'Fallback Filters') {
     return (
       <div>
-        {/* <div className="row w-100 position-relative m-0">
+        <div className="row w-100 position-relative m-0">
           <div className={`col-md-3 col-lg-2 web-filter d-none d-sm-block`}>{renderFilter()}</div>
           <div className={`col-md-9 col-lg-10`}>{renderProducts()}</div>
-        </div> */}
+        </div>
       </div>
     );
   } else {
