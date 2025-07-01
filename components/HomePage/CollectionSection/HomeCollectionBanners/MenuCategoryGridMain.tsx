2f9a25bd3f00606ad82425d39cd3c8b5ba1de2a5
Navneet <navneet@8848digital.com>
2025-07-01 16:29:04 +0530

    feat: add collection images

--- components/HomePage/CollectionSection/HomeCollectionBanners/MenuCategoryGridMain.tsx
+++ components/HomePage/CollectionSection/HomeCollectionBanners/MenuCategoryGridMain.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../../../../styles/components/menuCategoryGrid.module.scss';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const pastelColors = [
   '#FADADD', // pink
@@ -27,21 +28,26 @@ const spanClasses = [
 ];
 
 const MenuCategoryGridMain = ({ collectionData }: any) => {
+  const imageLoader = ({ src, width, quality }: { src: string; width: number; quality?: number }) => {
+    return `${src}?w=${width}&q=${quality || 75}`;
+  };
   return (
-    <div className="container py-3">
+    <div className="container py-3" >
       <div className={styles.gridContainer}>
         {collectionData?.slice(0, 9)?.map((item: any, index: number) => (
-          <div
+          <Link
             key={index}
+            href={`product-category/${item.DpCd}?page=1&currency=USD`}
             className={`${styles.gridItem} ${spanClasses[index] || ''}`}
             style={{ backgroundColor: pastelColors[index % pastelColors.length] }}
           >
+            <Image src={item?.imgUrl} alt="Product Image" fill style={{ objectFit: 'cover' }} loader={imageLoader} />
             <Link href={`product-category/${item.DpCd}?page=1&currency=USD`} className={styles.cardLink} aria-label={item?.DpCd}>
               <div className={styles.pastelCard}>
                 <span className={styles.cardText}>{item?.DpCd}</span>
               </div>
             </Link>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
