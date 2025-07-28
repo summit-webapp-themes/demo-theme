19e6995ddb6d9d3c7ee21c103dc68603fbc3662e
Navneet <navneet@8848digital.com>
2025-07-28 10:18:44 +0530

    feat: set language & currency from ls

--- _app.tsx
+++ _app.tsx
@@ -4,26 +4,46 @@ import type { AppProps } from 'next/app';
 import summitSettings from '../summit-settings.json';
 import { createFontImport } from '../utils/fontUtils';
 import dynamic from 'next/dynamic';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import { PersistGate } from 'redux-persist/integration/react';
 import { CONSTANTS } from '../services/config/app-config';
 import { persistor, store } from '../store/store';
 import useInitializeGoogleAnalytics from '../hooks/GoogleAnalytics/useInitializeGoogleAnalytics';
 import ErrorBoundary from '../components/ErrorBoundary';
-import useLanguageHandler from '../hooks/GeneralHooks/LanguageHandler';
 const Layout = dynamic(() => import('../components/Layout'));
 const ProtectedRoute = dynamic(() => import('../routes/ProtectedRoute'));
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.scss';
+import { setCurrencyValue } from '../store/slices/general_slices/multi-currency-slice';
+import { setLanguage } from '../store/slices/general_slices/multilingual-slice';
+import { currencyDisplayOptions, currencyOptions } from '../utils/addon-utils/currency-map';
+import { languageDisplayOptions } from '../utils/addon-utils/language-options';
+import { Option } from '../store/slices/general_slices/multilingual-slice';
+import useCurrencyLanguageHandler from '../hooks/GeneralHooks/LanguageHandler';
+
 const summitSettingsData: any = summitSettings;
 const fontFamily = summitSettingsData?.data?.font_family || 'Nunito';
 const dynamicFont = createFontImport(fontFamily);
 function InnerApp({ Component, pageProps }: AppProps) {
-  useLanguageHandler();
   const { ENABLE_GOOGLE_ANALYTICS, ALLOW_GUEST_TO_ACCESS_SITE_EVEN_WITHOUT_AUTHENTICATION } = CONSTANTS;
+  const { handleLanguageShallowUpdate, handleCurrencyShallowUpdate } = useCurrencyLanguageHandler();
+  
   useEffect(() => {
+    const storedCurrency = localStorage.getItem('selected_currency');
+    const storedLanguage = localStorage.getItem('selected_language');
+    const currency = currencyDisplayOptions.find((opt: Option) => storedCurrency && opt?.value === JSON?.parse(storedCurrency));
+    const language = languageDisplayOptions.find((opt: Option) => storedLanguage && opt?.label === JSON?.parse(storedLanguage));
+    
+    if (storedCurrency && currency) {
+      handleCurrencyShallowUpdate(currency);
+    }
+    
+    if (storedLanguage && language) {
+      handleLanguageShallowUpdate(language);
+    }
+
     if (ENABLE_GOOGLE_ANALYTICS) {
       useInitializeGoogleAnalytics();
     }
