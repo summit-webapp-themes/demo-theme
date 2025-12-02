6c472859fe7f8a3b4fa0c1979b362988cfd32009
nvnt122 <navneet@8848digital.com>
2025-12-02 14:35:58 +0530

    feat: enhance MenuCategoryGridMain and ImageGalleryWithBottomThumbnails for improved layout and image handling

--- components/HomePage/CollectionSection/HomeCollectionBanners/MenuCategoryGridMain.tsx
+++ components/HomePage/CollectionSection/HomeCollectionBanners/MenuCategoryGridMain.tsx
@@ -45,15 +45,26 @@ const MenuCategoryGridMain = ({ collectionData }: any) => {
   return (
     <div className="container py-3" >
       <div className={styles.gridContainer}>
-        {collectionDataWithImage?.slice(0, 9)?.map((item: any, index: number) => (
+        {collectionDataWithImage?.map((item: any, index: number) => (
           <Link
             key={index}
             href={`product-category/${item.DpCd}?page=1&currency=${currencyState.selected_currency_value}`}
-            className={`${styles.gridItem} ${spanClasses[index] || ''}`}
+            className={`${styles.gridItem} ${spanClasses[index % spanClasses.length]}`}
             style={{ backgroundColor: pastelColors[index % pastelColors.length] }}
           >
-            <Image src={item?.imgUrl} alt="Product Image" fill style={{ objectFit: 'cover' }} loader={imageLoader} />
-            <Link href={`product-category/${item.DpCd}?page=1&currency=${currencyState.selected_currency_value}`} className={styles.cardLink} aria-label={item?.DpCd}>
+            <Image
+              src={item?.imgUrl}
+              alt="Product Image"
+              fill
+              style={{ objectFit: 'cover' }}
+              loader={imageLoader}
+            />
+
+            <Link
+              href={`product-category/${item.DpCd}?page=1&currency=${currencyState.selected_currency_value}`}
+              className={styles.cardLink}
+              aria-label={item?.DpCd}
+            >
               <div className={styles.pastelCard}>
                 <span className={styles.cardText}>{t(item?.DpCd)}</span>
               </div>
